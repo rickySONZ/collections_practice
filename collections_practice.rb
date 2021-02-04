f79bf5b2aef5d436603a58084c861719ed0edaef
Ryan Erricson <ryanerricson@gmail.com>
2021-02-04 13:10:35 -0500

    commit tree 8def349dec4d486c64e1faf47c0a072b55f58cd6

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort { |x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+    array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |name|
+        name[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject(:+)
+end 
+
+def add_s(array)
+    array.collect.with_index do |word, index|
+        if array[index] != array[1]
+            word + "s"
+        else
+            word
+        end
+    end
+end
